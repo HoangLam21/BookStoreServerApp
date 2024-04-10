639c9567d37072e3d554be23624124054aa4a806
junkyedh <22520393@gm.uit.edu.vn>
2024-04-10 16:25:55 +0700

    AddRepositories

---
+++ BookStoreServerApp/src/main/java/com/team/bookstore/Repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.team.bookstore.Repositories;
+
+import com.team.bookstore.Entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,String> {
+}
