d4fe30accdb304dfa306627406bb9472ae500f81
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-05-08 10:21:25 +0700

    Complete Authority

--- BookStoreServerApp/src/main/java/com/team/bookstore/Configs/SecurityConfig.java
+++ BookStoreServerApp/src/main/java/com/team/bookstore/Configs/SecurityConfig.java
@@ -1,64 +1,80 @@
 package com.team.bookstore.Configs;
-
-import org.springframework.beans.factory.annotation.Value;
+import com.team.bookstore.Enums.Role;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.crypto.spec.SecretKeySpec;
-
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig{
+    @Autowired
+    JWTHS256Decoder jwths256Decoder;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    public static String[] PublicEndpoints = {"auth/**","author/all","author" +
+            "/find","book/all","book/find","category/all","category/find",
+            "customer/register","feedback/all","gallery/all","gallery/find",
+            "keyword/all","keyword/find","language/all","language/find",
+            "provider/all","provider/find","publisher/all","publisher/all"};
+    public static String[] CustomerEndpoints = {"/book/mine","/customer" +
+            "/myinfo","customer/my-payments","customer/my-orders","customer" +
+            "/update/**","/customer/create/info/**","/feedback/add",
+            "/feedback/all","/order/add","/order/cancel/**"};
+    public static String[] StaffEndpoints = {"/author/**",
+            "/language/**","/publisher/**","/provider/**","/staff/**","/order" +
+            "/all","/payment/all","/feedback/all",
+            "/keyword/**","/order/update-status/{id}","/import/**"};
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-    @Value("${jwt.signerKey}")
-    private String signerKey;
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(request ->
-                request.requestMatchers(HttpMethod.POST/*,publicendpoints*/).permitAll()
-                        .anyRequest().authenticated());
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PublicEndpoints).permitAll()
+                        .requestMatchers(CustomerEndpoints).hasAnyRole(Role.CUSTOMER.name(),Role.ADMIN.name())
+                        .requestMatchers(StaffEndpoints).hasAnyRole(Role.STAFF.name(),Role.ADMIN.name())
+                        .requestMatchers("/delete","/user").hasRole(Role.ADMIN.name())
+                        .requestMatchers("/message/loadchat").authenticated()
+                        .anyRequest().permitAll()
+                )
+                //.sessionManagement(sess -> sess.sessionCreationPolicy
+                // (SessionCreationPolicy.ALWAYS)
+                        //.invalidSessionUrl("/v1/login?expired")
+                        //.maximumSessions(1)
+                        //.expiredUrl("/v1/login?expired"))
+                .httpBasic(Customizer.withDefaults())
+                .logout(logout -> logout.logoutUrl("/v1/logout").invalidateHttpSession(true).clearAuthentication(true).deleteCookies("JSESSIONID"))
+                .headers(headers -> headers.cacheControl(HeadersConfigurer.CacheControlConfig::disable));
 
-        httpSecurity.oauth2ResourceServer(oauth2 ->
-                oauth2.jwt(jwtConfigurer ->
-                                jwtConfigurer.decoder(jwtDecoder())
-                                        .jwtAuthenticationConverter(jwtAuthenticationConverter()))
-                        .authenticationEntryPoint(new /*JwtAuthenticationEntryPoint()*/)
-        );
+        httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer
+                        .decoder(jwths256Decoder)
+                        .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                .authenticationEntryPoint(new JWTAuthenticationEntryPoint()));
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
-
         return httpSecurity.build();
-    }
 
+    }
     @Bean
-    JwtAuthenticationConverter jwtAuthenticationConverter(){
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
 
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
 
         return jwtAuthenticationConverter;
-    }
-
-    @Bean
-    JwtDecoder jwtDecoder(){
-        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
-        return NimbusJwtDecoder
-                .withSecretKey(secretKeySpec)
-                .macAlgorithm(MacAlgorithm.HS512)
-                .build();
     }
 }
