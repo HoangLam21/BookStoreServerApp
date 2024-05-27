814cf98d7720f81741b7318da48c602df4e5a935
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-05-27 17:10:11 +0700

    Done VNPAY

--- BookStoreServerApp/src/main/java/com/team/bookstore/Controllers/PaymentController.java
+++ BookStoreServerApp/src/main/java/com/team/bookstore/Controllers/PaymentController.java
@@ -9,10 +9,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/payment")
 @SecurityRequirement(name = "bearerAuth")
 public class PaymentController {
