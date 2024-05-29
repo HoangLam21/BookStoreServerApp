a00d3d88a5a8afcdf4516a437134b369809d1fe9
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-05-29 12:14:14 +0700

    Fix Cors Access

---
+++ BookStoreServerApp/src/main/java/com/team/bookstore/Configs/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.team.bookstore.Configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return source;
+    }
+}
