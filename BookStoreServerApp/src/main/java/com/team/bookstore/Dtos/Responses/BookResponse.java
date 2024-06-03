c04c4e2d165d668276a6c3d82c38b3f9559344c5
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-06-03 11:58:12 +0700

    return categoryId->categoryName

--- BookStoreServerApp/src/main/java/com/team/bookstore/Dtos/Responses/BookResponse.java
+++ BookStoreServerApp/src/main/java/com/team/bookstore/Dtos/Responses/BookResponse.java
@@ -34,7 +34,7 @@ public class BookResponse {
     Date updateAt;
     String createBy;
     String updateBy;
-    Category category_id;
+    String category;
     Set<Author> authors;
     Publisher publisher;
     Language language;
